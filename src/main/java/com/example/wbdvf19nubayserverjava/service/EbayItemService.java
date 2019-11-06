63220dbc5242102845b4e017f45abcebf8d8a5d7
Ron Sharma <sharma.ro@husky.neu.edu>
2019-11-06 15:22:28 -0500

    Replace spaces with %20 in ebayitem service

--- src/main/java/com/example/wbdvf19nubayserverjava/service/EbayItemService.java
+++ src/main/java/com/example/wbdvf19nubayserverjava/service/EbayItemService.java
@@ -15,9 +15,7 @@ public class EbayItemService {
     private URL httpsGETRequest;
 
     public EbayItemService() {
-        this.ebayFindingAPI = "https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME" +
-                "=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=RonitSha-NuBay-PRD-4b31d5c2d-dcfa3e9a" +
-                "&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=";
+        this.ebayFindingAPI = "https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=RonitSha-NuBay-PRD-4b31d5c2d-dcfa3e9a&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=";
     }
 
     public List<EbayItem> findAllItemsKeyword(String keyword) {
@@ -26,7 +24,7 @@ public class EbayItemService {
             return response;
         }
         try {
-            this.httpsGETRequest = new URL(this.ebayFindingAPI + keyword);
+            this.httpsGETRequest = new URL(this.ebayFindingAPI + keyword.replace(" ", "%20"));
 
             parseEbayJSON(response, this.httpsGETRequest);
         } catch (MalformedURLException m) {
