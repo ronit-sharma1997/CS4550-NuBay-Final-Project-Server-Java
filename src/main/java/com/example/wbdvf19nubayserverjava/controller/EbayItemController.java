09624672b4526a7aa65606b8a7e0dd987e0dc66e
Ron Sharma <sharma.ro@husky.neu.edu>
2019-11-06 15:02:31 -0500

    Adding logging tocontroller for debugging

--- src/main/java/com/example/wbdvf19nubayserverjava/controller/EbayItemController.java
+++ src/main/java/com/example/wbdvf19nubayserverjava/controller/EbayItemController.java
@@ -2,6 +2,8 @@ package com.example.wbdvf19nubayserverjava.controller;
 
 import com.example.wbdvf19nubayserverjava.model.EbayItem;
 import com.example.wbdvf19nubayserverjava.service.EbayItemService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,12 +16,15 @@ import java.util.List;
 public class EbayItemController {
     private EbayItemService itemService;
 
+    Logger logger = LoggerFactory.getLogger(EbayItemController.class);
+
     public EbayItemController() {
         this.itemService = new EbayItemService();
     }
 
     @GetMapping("/api/ebayItems/{keyword}")
     public List<EbayItem> findAllItemsKeyword(@PathVariable("keyword") String keyword) {
+        logger.info("Keyword entered" + keyword);
         return this.itemService.findAllItemsKeyword(keyword);
     }
 }
