c256a7bf957261f76f2783d0fb801ba62701401f
Ron Sharma <sharma.ro@husky.neu.edu>
2019-11-11 21:29:12 -0500

    Update CORS permission to allow any site to access server

--- src/main/java/com/example/wbdvf19nubayserverjava/controller/EbayItemController.java
+++ src/main/java/com/example/wbdvf19nubayserverjava/controller/EbayItemController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class EbayItemController {
     private EbayItemService itemService;
 
